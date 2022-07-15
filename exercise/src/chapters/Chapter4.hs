b28b951046e75e705dff37127d055f219853446d
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-07-15 17:38:32 +0900

    feat(Chapter4): Solving exercise 3

--- exercise/src/chapters/Chapter4.hs
+++ exercise/src/chapters/Chapter4.hs
@@ -21,3 +21,27 @@ safetail' xs | null xs = error "Cannot use empty list for this function (2-b)"
 safetail'' :: [a] -> [a]
 safetail'' [] = error "Cannot use empty list for this function (2-c)"
 safetail'' (_:xs) = xs
+
+-- 3-1
+(|||) :: Bool -> Bool -> Bool
+True ||| True = True
+True ||| False = True
+False ||| True = True
+False ||| False = False
+
+-- 3-2
+(||||) :: Bool -> Bool -> Bool
+False |||| False = False
+_ |||| _ = True
+
+-- 3-3
+(|||||) :: Bool -> Bool -> Bool
+False ||||| b = b
+True ||||| _ = True
+
+-- 3-4
+(||||||) :: Bool -> Bool -> Bool
+b |||||| c | b = True
+          | c = True
+          | otherwise = False
+
