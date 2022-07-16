da58eb0393acb4004ae00094afd11adfd9151328
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-07-17 03:36:02 +0900

    feat(Chapter7): Solving exercises 1~2

---
+++ exercise/src/chapters/Chapter7.hs
@@ -0,0 +1,33 @@
+module Chapter7
+    ( 
+    ) where
+
+-- 1
+asIsMapFilter f p = \xs -> [f x | x <- xs, p x]
+toBeMapFilter f p = \xs -> (map f . filter p) xs
+
+-- 2 - all
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = foldr (\a1 a2 -> p a1 && a2) True
+
+-- 2 - any
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = foldr (\a1 a2 -> p a1 || a2) False
+
+-- 2 - takeWhile
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p [] = []
+takeWhile' p (x:xs)
+    | p x = x : takeWhile' p xs
+    | otherwise = []
+
+-- 2 - dropWhile
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p [] = []
+dropWhile' p (x:xs)
+    | p x = dropWhile' p xs
+    | otherwise = x:xs
+
+
+
+
