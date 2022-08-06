ee21ea3089ceb482426869fe100da1b8f9422ee5
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-06 23:17:01 +0900

    feat(NimGame): print stars at console

--- exercise/src/chapters/Chapter9/NimGame.hs
+++ exercise/src/chapters/Chapter9/NimGame.hs
@@ -4,3 +4,23 @@ module NimGame
 
 import Parsing -- if you run in ghci, then you have to do ":set -isrc/chapters/Chapter8"
 import Tui
+
+type StarCount = Int
+type StarBoard = [StarCount]
+
+starBoard :: StarBoard
+starBoard = [5,4,3,2,1]
+
+echoStarBoard :: StarBoard -> IO ()
+echoStarBoard b = seqn [do putStr $ show index ++ ": "
+                           echoStar starCnt
+                           putStrLn ""
+                        | (index, starCnt) <- zip [1..] b]
+
+echoStar :: StarCount -> IO ()
+echoStar starCnt = seqn [putStr "*" | _ <- [1..starCnt]]
+
+startNimGame :: IO ()
+startNimGame = do putStrLn "Nim game start !"
+                  echoStarBoard starBoard
+
