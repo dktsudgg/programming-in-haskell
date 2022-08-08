d2e39e745f39962b5458dc23939388c7f4f1d48d
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-08 21:47:23 +0900

    feat: NimGame

--- exercise/src/chapters/Chapter9/NimGame.hs
+++ exercise/src/chapters/Chapter9/NimGame.hs
@@ -20,7 +20,34 @@ echoStarBoard b = seqn [do putStr $ show index ++ ": "
 echoStar :: StarCount -> IO ()
 echoStar starCnt = seqn [putStr "*" | _ <- [1..starCnt]]
 
+type Gamer = String
+type Gamers = [Gamer]
+
+gamers :: Gamers
+gamers = ["User1", "User2"]
+
+nextTurn :: Int -> Gamers -> Gamer
+nextTurn curCycle gamers = gamers !! (curCycle `mod` length gamers)
+
+nextBoard :: Int -> StarBoard -> StarBoard
+nextBoard choice board = take (choice - 1) board ++ [(board !! (choice - 1)) - 1] ++ drop choice board
+
+isEnd :: StarBoard -> Bool
+isEnd = foldr (\a b -> a == 0 && b) True
+
 startNimGame :: IO ()
-startNimGame = do putStrLn "Nim game start !"
-                  echoStarBoard starBoard
+startNimGame = nimGame 0 starBoard
+
+nimGame :: Int -> StarBoard -> IO ()
+nimGame curCycle board = do if isEnd board then
+                                do putStrLn $ nextTurn curCycle gamers ++ " lost the game.."
+                                   return ()
+                            else do echoStarBoard board
+                                    putStr $ nextTurn curCycle gamers ++ "'s choice: "
+                                    numStr <- readLine ""
+                                    do case parse natural numStr of
+                                            [(num, _)]  -> nimGame (curCycle + 1) (nextBoard num board)
+                                            _           -> do putStrLn "Illegal input.. try again !"
+                                                              nimGame curCycle board
+                                    return ()
 
