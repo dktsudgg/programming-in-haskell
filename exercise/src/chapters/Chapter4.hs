a8f794d8cf9e4b7426e4ce018e6ed129c7cc1b07
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-12 23:16:01 +0900

    refactor(Chapter4): indent

--- exercise/src/chapters/Chapter4.hs
+++ exercise/src/chapters/Chapter4.hs
@@ -4,18 +4,20 @@ module Chapter3
 
 -- 1
 halve :: [a] -> ([a], [a])
-halve xs = (take halfCnt xs, drop halfCnt xs)
-    where halfCnt = length xs `div` 2
+halve xs = (take halfCnt xs, drop halfCnt xs) 
+  where halfCnt = length xs `div` 2
 
 -- 2-a
 safetail :: [a] -> [a]
-safetail xs = if null xs then error "Cannot use empty list for this function (2-a)"
-              else tail xs
+safetail xs =
+    if null xs then error "Cannot use empty list for this function (2-a)"
+    else tail xs
 
 -- 2-b
 safetail' :: [a] -> [a]
-safetail' xs | null xs = error "Cannot use empty list for this function (2-b)"
-             | otherwise = tail xs
+safetail' xs
+    | null xs = error "Cannot use empty list for this function (2-b)"
+    | otherwise = tail xs
 
 -- 2-c
 safetail'' :: [a] -> [a]
@@ -41,9 +43,10 @@ True ||||| _ = True
 
 -- 3-4
 (||||||) :: Bool -> Bool -> Bool
-b |||||| c | b = True
-          | c = True
-          | otherwise = False
+b |||||| c
+    | b = True
+    | c = True
+    | otherwise = False
 
 -- 6
 mult = \x -> \y -> \z -> x * y * z
