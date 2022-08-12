dd17285c875e0added99b85efb43613f89d2b58b
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-12 23:15:52 +0900

    refactor(Chapter2): indent

--- exercise/src/chapters/Chapter2.hs
+++ exercise/src/chapters/Chapter2.hs
@@ -4,9 +4,9 @@ module Chapter2
 
 -- 3
 n = a `div` length xs
-    where
-        a = 10
-        xs = [1, 2, 3, 4, 5]
+  where
+    a = 10
+    xs = [1, 2, 3, 4, 5]
 
 -- 4
 last' xs = head (reverse xs)
