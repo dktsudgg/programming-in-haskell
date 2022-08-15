9186641ee6e07816d72be6f3b0e77542ac058e48
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-15 15:46:06 +0900

    feat(Chapter10): Solving exercise 2

--- exercise/src/chapters/Chapter10.hs
+++ exercise/src/chapters/Chapter10.hs
@@ -25,3 +25,20 @@ mult m n = add m (mult m nMinus1)
   where
     nMinus1 = int2nat $ nat2int n - 1
 
+-- 2
+data Tree = Leaf Int | Node Tree Int Tree
+
+t :: Tree
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Int -> Tree -> Bool
+occurs m (Leaf n) = m == n
+-- occurs m (Node l n r) = m == n || occurs m l || occurs m r
+-- occurs m (Node l n r)
+--     | m == n = True
+--     | m < n = occurs m l
+--     | otherwise = occurs m r
+occurs m (Node l n r) = case compare m n of
+    EQ -> True
+    LT -> occurs m l
+    GT -> occurs m r
