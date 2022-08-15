fd421082bd80d3945e46157517146f67337a4228
dktsudgg <dktsudgg@yonsei.ac.kr>
2022-08-15 16:01:36 +0900

    feat(Chapter10): Solving exercise 3

--- exercise/src/chapters/Chapter10.hs
+++ exercise/src/chapters/Chapter10.hs
@@ -29,7 +29,7 @@ mult m n = add m (mult m nMinus1)
 data Tree = Leaf Int | Node Tree Int Tree
 
 t :: Tree
-t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Node (Leaf 8) 9 (Node (Leaf 10) 11 (Leaf 12))))
 
 occurs :: Int -> Tree -> Bool
 occurs m (Leaf n) = m == n
@@ -42,3 +42,13 @@ occurs m (Node l n r) = case compare m n of
     EQ -> True
     LT -> occurs m l
     GT -> occurs m r
+
+-- 3
+cntOfLeafIn :: Tree -> Int
+cntOfLeafIn (Leaf _) = 1
+cntOfLeafIn (Node l n r) = cntOfLeafIn l + cntOfLeafIn r
+
+isBalanced :: Tree -> Bool
+isBalanced (Leaf _) = True
+isBalanced (Node l n r) = diff <= 1
+  where diff = abs $ cntOfLeafIn l - cntOfLeafIn r
